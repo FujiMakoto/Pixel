907c2c34bf1ebccc0cd0b5e26a1df09b9a0bd393
Makoto <FujiMakoto@users.noreply.github.com>
2015-02-09 08:39:27 -0500

    Repository ArrayAccess, Arrayable, Jsonable, JsonSerializable implementations

--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -1,10 +1,12 @@
 <?php namespace Pixel\Repositories;
 
 use Carbon\Carbon;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Support\Jsonable;
 //use Illuminate\Pagination\LengthAwarePaginator as Paginator;
 use Illuminate\Pagination\Paginator as Paginator;
 
-abstract class Repository {
+abstract class Repository implements \ArrayAccess, Arrayable, Jsonable, \JsonSerializable {
 
     /**
      * The repositories attributes
@@ -116,6 +118,16 @@ abstract class Repository {
     }
 
     /**
+     * Convert the object into something JSON serializable.
+     *
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        return $this->toArray();
+    }
+
+    /**
      * Convert the repository instance to an array.
      *
      * @return array
@@ -168,6 +180,51 @@ abstract class Repository {
     public function __set($key, $value)
     {
         $this->setAttribute($key, $value);
+    }
+
+    /**
+     * Determine if the given attribute exists.
+     *
+     * @param  mixed  $offset
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->$offset);
+    }
+
+    /**
+     * Get the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return $this->$offset;
+    }
+
+    /**
+     * Set the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @param  mixed  $value
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->$offset = $value;
+    }
+
+    /**
+     * Unset the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->$offset);
     }
 
     /**
