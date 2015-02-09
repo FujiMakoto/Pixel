934b5724549a65bd92306cae9e91927bad726586
Makoto <FujiMakoto@users.noreply.github.com>
2015-02-09 12:33:59 -0500

    Image Toolbar

--- resources/views/images/show.blade.php
+++ resources/views/images/show.blade.php
@@ -3,6 +3,11 @@
 {{-- Color Scheme --}}
 @section('color-scheme'){{ $image->getColorScheme() }}@stop
 
+@section('styling')
+    @include('images/_partials/accentuation')
+@stop
+
+{{-- Page Content --}}
 @section('content')
     <div class="container image-show-container">
         {{-- Image Preview --}}
@@ -10,6 +15,29 @@
             <div class="image-preview">
                 {{-- Preview Image --}}
                 {!! HTML::image($image->getUrl($image::PREVIEW), $image->name, ['id' => 'preview']) !!}
+
+                {{-- Image Toolbar --}}
+                <div class="btn-toolbar pull-right fade" role="toolbar">
+                    <div class="btn-group">
+                        {{-- Download Image --}}
+                        <a href="{{ $image->getUrl(null, ['download' => 1]) }}" class="btn btn-default download" download="{{ $image->name }}">
+                            <i class="fa fa-download fw"></i>
+                        </a>
+
+                        {{-- Image Options --}}
+                        @if ($canEdit)
+                            <button type="button" class="btn btn-default dropdown-toggle" id="image-options" data-toggle="dropdown">
+                                <i class="fa fa-cog fw"></i>
+                            </button>
+
+                            <ul class="dropdown-menu" role="menu" aria-labelledby="image-options">
+                                <li role="presentation">
+                                    <a href="#" role="menuitem" tabindex="-1"><i class="fa fa-trash-o fa-fw"></i> Delete</a>
+                                </li>
+                            </ul>
+                        @endif
+                    </div>
+                </div>
             </div>
         </div>
 
